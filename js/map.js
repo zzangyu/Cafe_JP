d54c8fda182270e8ad219b6f29835e3da3dc29e5
zzangyu <hellohan95@gmail.com>
2022-11-28 20:03:40 +0900

    slide ui

--- js/map.js
+++ js/map.js
@@ -1,6 +1,6 @@
 function fetchPage(lngs, lats){
     $.ajax({
-  	url: 'https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=79eb47654869941a&keyword=cafe&count=100&format=jsonp&range=5&lng='+lngs+'&lat='+lats,
+  	url: 'https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=79eb47654869941a&keyword=cafe&count=100&format=jsonp&range=3&lng='+lngs+'&lat='+lats,
   	type: 'GET',
   	dataType: 'jsonp',
 	}).done(function(data) {
@@ -10,6 +10,12 @@ function fetchPage(lngs, lats){
 		var name = shop.map(function(element){
   			return `${element.name}`;
 		})
+		var namekana = shop.map(function(element){
+			return `${element.name_kana}`;
+	 	})
+		var midnight = shop.map(function(element){
+			return `${element.midnight}`;
+	 	})
 		var wifi = shop.map(function(element){
   			return `${element.wifi}`;
 		})
@@ -19,23 +25,42 @@ function fetchPage(lngs, lats){
 		var lng = shop.map(function(element){ // 경도
   			return `${element.lng}`;
 		})
-		var urls = shop.map(function(element){
-  			return `${element.urls.pc}`;
-		})
-		var order = shop.map(function(element){
+		// var urls = shop.map(function(element){
+  		// 	return `${element.urls.pc}`;
+		// })
+		var photo = shop.map(function(element){
 			return `${element.photo.pc.l}`;
 	 	})
+		var logo_image = shop.map(function(element){
+			return `${element.logo_image}`;
+	 	})
+		var logo_none = 'https://imgfp.hotp.jp/SYS/cmn/images/common/diary/custom/m30_img_noimage.gif';
+		var check = true;
+		if(name[0] === undefined) {
+			check = false;
+		}
+		console.log(check);
 		var i = 0;
 		var tags = "";
-		while(i < name.length) {			
-			tags = tags + '<div class="cafe_slide"><img scr="'+order[i]+'"></div>'
-			i = i + 1;
+		if(check) {
+			while(i < name.length) {
+				if(logo_image[i] == logo_none) {
+					logo_image[i] = '../img/none.jpeg';
+				}
+				tags = tags + '<div class="cafe_slide"><div><img src="'+logo_image[i]+'"><h3 class="cafe_name" onclick="openModal('+i+')">'+name[i]+'</h3></div></div>'
+				+'<div class="modal" id="'+i+'"><div class="modal_body">'
+				+'<h3 class="modal_title">'+name[i]+'<button class="exit_bt" onclick="exitModal('+i+')">X</button></h3>'
+				+'</div></div>';
+				i = i + 1;
+			}
+		} else {
+			tags = '<div id="misenai"><img src="../img/uu.jpg"><h3>この辺には店がありません。</h3></div>';
+			document.querySelector('.cafe_slide_wrap').style.height = '100%';
 		}
 		document.querySelector('.cafe_slide_wrap').innerHTML = tags;
-		
+
+		  // 맵 다시 로드
 		mapboxgl.accessToken = 'pk.eyJ1IjoiaGFuY2hhbmd5dSIsImEiOiJjbDZmb3h4d2Iwa2VnM2pvcjh3ZWI0c3h5In0.3sdZR_Tj6t5ZHWJgOinBdQ';
-		// 맵 다시 로드
-		
 		const map = new mapboxgl.Map({
 			container: 'map', // container ID
 		    style: 'mapbox://styles/hanchangyu/cl6g6vdi3000215nwl6y2wchb', // style URL
@@ -68,7 +93,7 @@ function fetchPage(lngs, lats){
 				.setPopup(
 				new mapboxgl.Popup({ offset: 25 }) // add popups
 					.setHTML(
-					`<h3>${feature.properties.title}</h3><a href="${urls[j]}" target="#">Hotpepper Page</a>`
+					`<h3>${feature.properties.title}</h3>`
 					)
 				)
 			.addTo(map);
@@ -80,10 +105,20 @@ function fetchPage(lngs, lats){
 			var center_lat = center.lat; 
 			var center_lng = center.lng; 
 			fetchPage(center_lng, center_lat);
-			// document.querySelector('#kokode_bt').innerHTML = '<div onclick="fetchPage('+center.lng+', '+center.lat+')">ここを探そう</div>'
 		});
 			
 	}).fail(function() {
 		console.log('fail')
 	});
+}
+// modal
+function openModal(index) {
+	var modal = index;
+	var mo = '#'+modal;
+	$(mo).css('display','flex').hide().fadeIn();
+}
+function exitModal(index) {
+	var modal = index;
+	var mo = '#'+modal;
+	$(mo).fadeOut();
 }
